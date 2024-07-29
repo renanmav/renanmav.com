153852580747ef5eda7d2009b6771e9c23471cc7
Renan Machado <renan.machado@coinbase.com>
2024-07-29 14:00:22 -0300

    add footer margin bottom

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer>
+    <footer className="mb-20">
       <div className="my-12 w-12 border-b border-neutral-300 dark:border-neutral-800" />
       <p className="text-sm text-gray-400">
         Recording memories while time passes by...
