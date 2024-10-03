7e489bf53650d900f7582d2806b7607ac498793e
Renan Mav <renan.mav@hotmail.com>
2024-10-03 20:49:55 -0300

    implement lateral index with smooth scroll

--- app/components/lateral-index.tsx
+++ app/components/lateral-index.tsx
@@ -64,6 +64,22 @@ export default function LateralIndex() {
     }
   };
 
+  const handleLinkClick = (
+    event: React.MouseEvent<HTMLAnchorElement>,
+    id: string,
+  ) => {
+    event.preventDefault();
+    if (headings[0]?.id === id) {
+      // If it's the first heading, scroll to the top
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    } else {
+      const targetElement = document.getElementById(id);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
+
   return (
     <aside className="fixed left-10 top-6">
       <nav className="hidden lg:block">
@@ -72,6 +88,7 @@ export default function LateralIndex() {
             <li key={heading.id} className={getLevelClassName(heading.level)}>
               <a
                 href={`#${heading.id}`}
+                onClick={(event) => handleLinkClick(event, heading.id)}
                 className="text-sm text-gray-400 transition-colors duration-300 hover:text-gray-600 dark:hover:text-gray-200"
               >
                 {heading.text}
