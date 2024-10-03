6a7792f82ac14d3f8ba6c783b8d6566eea61dbea
Renan Mav <renan.mav@hotmail.com>
2024-10-02 23:41:53 -0300

    update tailwind and og route

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -1,36 +1,36 @@
 import { myInfo } from "app/constants";
+import { baseUrl } from "app/sitemap";
 import { ImageResponse } from "next/og";
 
 export function GET(request: Request) {
-  let url = new URL(request.url);
-  let title = url.searchParams.get("title") || myInfo.name;
-
-  // <div
-  //   tw="flex flex-col w-full h-full items-center justify-center"
-  //   style={{
-  //     backgroundImage: "url('/unfurl-blog-post.png')",
-  //     backgroundSize: "cover",
-  //     backgroundPosition: "center",
-  //   }}
-  // >
-  //   <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-  //     <h2
-  //       tw="flex flex-col text-4xl font-bold text-left text-white"
-  //       style={{
-  //         textShadow:
-  //           "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
-  //       }}
-  //     >
-  //       {title}
-  //     </h2>
-  //   </div>
-  // </div>
+  const url = new URL(request.url);
+  const title = url.searchParams.get("title") || myInfo.name;
+  const backgroundImageUrl = baseUrl + "/unfurl-blog-post.png";
 
   return new ImageResponse(
     (
-      <div tw="flex flex-col w-full h-full items-center justify-center bg-red">
-        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2 tw="flex flex-col text-4xl font-bold text-left text-white">
+      <div
+        style={{
+          backgroundImage: `url('${backgroundImageUrl}')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8 flex-wrap">
+          <h2
+            style={{
+              fontSize: "48px",
+              color: "#fff",
+              textShadow:
+                "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+            }}
+          >
             {title}
           </h2>
         </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,6 @@ const config: Config = {
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       textColor: {
         black: "rgb(20, 20, 20)",
       },
