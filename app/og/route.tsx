9de51742adae8d41b471297eeab41e5131c518ad
Renan Mav <renan.mav@hotmail.com>
2024-10-02 23:16:48 -0300

    change og route

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -5,24 +5,32 @@ export function GET(request: Request) {
   let url = new URL(request.url);
   let title = url.searchParams.get("title") || myInfo.name;
 
+  // <div
+  //   tw="flex flex-col w-full h-full items-center justify-center"
+  //   style={{
+  //     backgroundImage: "url('/unfurl-blog-post.png')",
+  //     backgroundSize: "cover",
+  //     backgroundPosition: "center",
+  //   }}
+  // >
+  //   <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+  //     <h2
+  //       tw="flex flex-col text-4xl font-bold text-left text-white"
+  //       style={{
+  //         textShadow:
+  //           "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
+  //       }}
+  //     >
+  //       {title}
+  //     </h2>
+  //   </div>
+  // </div>
+
   return new ImageResponse(
     (
-      <div
-        tw="flex flex-col w-full h-full items-center justify-center"
-        style={{
-          backgroundImage: "url('/unfurl-blog-post.png')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
+      <div tw="flex flex-col w-full h-full items-center justify-center bg-red">
         <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-          <h2
-            tw="flex flex-col text-4xl font-bold text-left text-white"
-            style={{
-              textShadow:
-                "-1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000",
-            }}
-          >
+          <h2 tw="flex flex-col text-4xl font-bold text-left text-white">
             {title}
           </h2>
         </div>
