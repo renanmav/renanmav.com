ca36387bce2ed5f0061ebef46f8a693f0fa5bcd7
Renan Mav <renan.mav@hotmail.com>
2024-10-04 14:05:16 -0300

    add resume to contact page

--- app/(public)/contact/page.tsx
+++ app/(public)/contact/page.tsx
@@ -1,3 +1,5 @@
+import Link from "app/components/link";
+
 import YupThatsMe from "./yup-thats-me";
 import ContactList from "./contact-list";
 
@@ -10,8 +12,18 @@ export default function ContactPage() {
   return (
     <section>
       <h1 className="mb-4 text-base font-semibold">Shoot me a message 💬</h1>
-      <p className="mb-4 text-base">
+      <p className="text-base">
         Keep in mind that email communication is preferred.
+      </p>
+      <p className="mb-4 text-base">
+        Also,{" "}
+        <Link
+          href="https://drive.google.com/file/d/1za5Dwa52EK6TRYqB10Uib3AchAZO-Kv_/view?usp=sharing"
+          openInNewTab
+        >
+          here's a copy of my resume
+        </Link>
+        .
       </p>
       <ContactList />
       <YupThatsMe />
