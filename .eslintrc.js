812ed5f053c6f708f2641b9162ec99bba61a11e2
Renan Mav <renan.mav@hotmail.com>
2024-10-22 14:26:04 -0300

    enforce eslint file naming

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,20 @@
 const config = {
   root: true,
   extends: ["next/core-web-vitals", "plugin:tailwindcss/recommended"],
+  plugins: ["filenames"],
   rules: {
     "react/no-unescaped-entities": "off",
   },
+  overrides: [
+    {
+      files: ["app/components/*.tsx"],
+      rules: {
+        "filenames/match-exported": "error",
+        "filenames/no-index": "error",
+        "filenames/match-regex": ["error", "^[A-Z][a-zA-Z0-9]*\\.tsx$", true],
+      },
+    },
+  ],
 };
 
 module.exports = config;
